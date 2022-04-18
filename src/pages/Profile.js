256f6ddcfef4cb2b70c1a3d6210b0b852a917d8a
aarongn85 <103927783+aarongn85@users.noreply.github.com>
2022-04-18 01:57:07 -0700

    Add files via upload

---
+++ src/pages/Profile.js
@@ -0,0 +1,17 @@
+import Header from '../components/shared/header/header';
+import Footer from '../components/shared/footer/footer';
+import '../pages/Profile.css';
+
+function Profile() {
+  return (
+      <div>
+        <Header />
+          <main className="row">
+            <h1>Profile of</h1>
+          </main>
+        <Footer />
+      </div>
+  );
+}
+
+export default Profile;
